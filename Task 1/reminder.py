0b276572f5fd7dc0e2b05172b30b05bf53474a74
jinzezhu0609-ops <jinzezhu0609@gmail.com>
2026-03-06 23:32:42 +0800

    Update reminder.py
    add comments

--- Task 1/reminder.py
+++ Task 1/reminder.py
@@ -1,21 +1,22 @@
 class Reminder:
-    REMIND_BEFORE_SECONDS = 5 * 60
+    REMIND_BEFORE_SECONDS = 300 # Remind 5 minutes before the start
 
     @staticmethod
     def check_reminders(reservations):
         messages = []
 
-        for res in reservations:
-            if not res.is_active():
+        for res in reservations:   
+            if not res.is_active():  # Skip inactive reservations
                 continue
 
-            seconds_to_start = res.time_to_start()
+            seconds_to_start = res.time_to_start()   # Calculate time until reservation starts
             if 0 < seconds_to_start <= Reminder.REMIND_BEFORE_SECONDS:
                 messages = f"[REMIND] Seat {res.seat.seat_id} will start soon!"
                 messages.append(messages)
 
-            seconds_to_end = res.time_to_end()
+            seconds_to_end = res.time_to_end()  # Calculate time reservation ends
             if 0 < seconds_to_end <= Reminder.REMIND_BEFORE_SECONDS:
                 messages = f"[REMIND] Seat {res.seat.seat_id} will end soon, please prepare to leave."
                 messages.append(messages)
-        return messages
+
+        return messages  # return all collected reminders
