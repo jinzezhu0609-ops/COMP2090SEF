2e457eb48ece1990573a49e540dcd71998ded74f
jinzezhu0609-ops <jinzezhu0609@gmail.com>
2026-03-30 02:16:14 +0800

    Update main.py

--- Task 1/main.py
+++ Task 1/main.py
@@ -1,6 +1,1 @@
-from system import LibrarySystem  # Import LibrarySystem from system module
 
-if __name__ == "__main__":  
-    system = LibrarySystem()
-
-    system.run() # Start and run the main library system
