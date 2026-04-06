45c5775ae9d2ea0ad9434e71d79950083a7dc7ec
jinzezhu0609-ops <jinzezhu0609@gmail.com>
2026-04-06 21:34:22 +0800

    Add files via upload

--- Task 1/reminder.py
+++ Task 1/reminder.py
@@ -1,25 +1,30 @@
 class Reminder():
+    '''Class for managing system reminder'''
     remind_Before_Seconds = 300 # Remind 5 minutes before the start
 
     @staticmethod
     def check_reminders(reservations):
+        '''Check all reservations and generate reminder messages.'''
         messages = []
-
+        
         for res in reservations:   
             if not res.is_active():  # Skip inactive reservations
                 continue
 
+            # Check if the reservation has just officially started
             if res.is_past_start() and not res.ongoing_notified:
                 messages_1 = f"[REMIND] Seat {res.seat.seat_id} reservation time is officially beginning"
                 messages.append(messages_1)
                 res.ongoing_notified = True
 
+            # Check if the reservation is starting soon
             seconds_to_start = res.time_to_start()   # Calculate time until reservation starts
             if 0 < seconds_to_start <= Reminder.remind_Before_Seconds and not res.start_notified:
                 messages_2 = f"[REMIND] Seat {res.seat.seat_id} will start soon!"
                 messages.append(messages_2)
                 res.start_notified = True
-
+                
+            # Check if the reservation is ending soon
             seconds_to_end = res.time_to_end()  # Calculate time reservation ends
             if 0 < seconds_to_end <= Reminder.remind_Before_Seconds and not res.end_notified:
                 messages_3 = f"[REMIND] Seat {res.seat.seat_id} will end soon, please prepare to leave."
